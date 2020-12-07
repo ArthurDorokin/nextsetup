cc7907a83c5fddb8e95a20457d71f0c9d2bd0a28
Artur <a.u.dorokhin@gmail.com>
2020-12-07 20:20:44 +0200

    finish

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,33 @@
-import React, {useState} from 'react';
+import React, {useEffect} from 'react';
 import Head from 'next/head'
-import {NavLink} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchPosts} from "../store/actions/postAction";
-import {useEffect} from "react";
 
-export default function Home() {
+export async function getStaticProps() {
+    const resp = await fetch('https://api.github.com/users?per_page=100')
+    const json = await resp.json()
+    return {
+        props: {
+            json
+        }
+    }
+}
+
+export default function Home(json) {
     const dispatch = useDispatch();
     const posts = useSelector(state => state.postData.posts);
 
-    useEffect(() => {
-        async function load() {
-            const resp = await fetch('https://api.github.com/users?per_page=100')
-            const json = await resp.json()
-            // setData(json)
-            dispatch(fetchPosts(json))
-        }
 
-        load()
+    useEffect(() => {
+        dispatch(fetchPosts(json.json))
     }, [])
+
 
 
     return (
         <div>
             <Head>
-                <title>Create Next App</title>
+                <title>Cr1eate Next App</title>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
 
--- store/actions/postAction.js
+++ store/actions/postAction.js
@@ -1,6 +1,3 @@
-import * as types from '../types'
-import axios from 'axios';
-
 export const fetchPosts = (json) => ({
     type: "FETCH_POSTS",
     payload: json
